3aee815e329b5913d27c8ec2f5b3190f92c027b7
luzz <lizazz@yandex.ru>
2021-02-13 13:09:15 +0200

    add bottomSheet dialog

--- lib/main.dart
+++ lib/main.dart
@@ -13,18 +13,27 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext inContext) {
      _showIt() {
-       return Scaffold.of(inContext).showSnackBar(
-         SnackBar(
-           backgroundColor: Colors.red,
-           duration: Duration(seconds: 5),
-           content: Text("I like pie!"),
-           action: SnackBarAction(
-             label: "Chow down",
-             onPressed: () {
-               print("Getting' fat");
-             },
-           ),
-         )
+       showModalBottomSheet(context: inContext,
+           builder: (BuildContext inContext) {
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text("What's your favorite pet?"),
+                  FlatButton(
+                      onPressed: () {Navigator.of(inContext).pop();},
+                      child: Text("Dog")
+                  ),
+                  FlatButton(
+                      onPressed: () {Navigator.of(inContext).pop();},
+                      child: Text("Cat")
+                  ),
+                  FlatButton(
+                      onPressed: () {Navigator.of(inContext).pop();},
+                      child: Text("Ferret")
+                  )
+                ],
+              );
+           }
        );
     }
 
