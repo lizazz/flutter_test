7c1372034486b68ea4ba86f57f7b6b08f9a7272d
luzz <lizazz@yandex.ru>
2021-02-19 09:23:42 +0200

    add ListView

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
-      body: GridView.count(
-        padding: EdgeInsets.all(4.0),
-        crossAxisCount: 4,
-        childAspectRatio: 1.0,
-        mainAxisSpacing: 4.0,
-        crossAxisSpacing: 4.0,
+      body: ListView(
         children: [
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo()),
-          GridTile(child: new FlutterLogo())
+          ListTile(leading: Icon(Icons.gif), title: Text("1")),
+          ListTile(leading: Icon(Icons.book), title: Text("2")),
+          ListTile(leading: Icon(Icons.call), title: Text("3")),
+          ListTile(leading: Icon(Icons.dns), title: Text("4")),
+          ListTile(leading: Icon(Icons.cake), title: Text("5")),
+          ListTile(leading: Icon(Icons.pets), title: Text("6")),
+          ListTile(leading: Icon(Icons.poll), title: Text("7")),
+          ListTile(leading: Icon(Icons.face), title: Text("8")),
+          ListTile(leading: Icon(Icons.home), title: Text("9")),
+          ListTile(leading: Icon(Icons.adb), title: Text("10")),
+          ListTile(leading: Icon(Icons.hd), title: Text("11")),
+          ListTile(leading: Icon(Icons.dvr), title: Text("12")),
+          ListTile(leading: Icon(Icons.toc), title: Text("13")),
+          ListTile(leading: Icon(Icons.tv), title: Text("14")),
+          ListTile(leading: Icon(Icons.gif), title: Text("15")),
         ],
       )
     ),
