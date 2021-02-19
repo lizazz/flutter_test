1909828965bd2434011860c89568d05a3f55e19e
luzz <lizazz@yandex.ru>
2021-02-19 09:17:44 +0200

    add GridView

--- lib/main.dart
+++ lib/main.dart
@@ -7,37 +7,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold(
-      body: Column(
-          children: [
-            Container(height: 100),
-            DataTable(
-                sortColumnIndex: 1,
-                columns: [
-                  DataColumn(label: Text('First name')),
-                  DataColumn(label: Text("Last Name"))
-                ], 
-                rows: [
-                  DataRow(
-                      cells: [
-                        DataCell(Text("Leia")),
-                        DataCell(Text("Organa"), showEditIcon: true)
-                      ]
-                  ),
-                  DataRow(
-                    cells: [
-                      DataCell(Text("Luke")),
-                      DataCell(Text("Skywalker"))
-                    ]
-                  ),
-                  DataRow(
-                    cells: [
-                      DataCell(Text("Han")),
-                      DataCell(Text("Solo"))
-                    ]
-                  )
-                ]
-            )
-          ])
+      body: GridView.count(
+        padding: EdgeInsets.all(4.0),
+        crossAxisCount: 4,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 4.0,
+        crossAxisSpacing: 4.0,
+        children: [
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo()),
+          GridTile(child: new FlutterLogo())
+        ],
+      )
     ),
     );
   }
